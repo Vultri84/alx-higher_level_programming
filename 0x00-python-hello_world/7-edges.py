d1ba6cbd9f250e8a9ffbd4eba684962fe2cf6391
Vultri84 <vultri84@gmail.com>
2023-01-04 16:22:09 -0800

    Changed permissions

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[0:3]
+word_last_2 = word[-2:]
+middle_word = word[1:7]
+print(f"First 3 letters: {word[0:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:7]}")
+
